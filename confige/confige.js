4c95b7cfb8c6f1439503d66e75bcea15dcf42ccf
Saveliy55 <122668823+Saveliy55@users.noreply.github.com>
2023-12-30 13:12:39 +0600

    Update confige.js

--- confige/confige.js
+++ confige/confige.js
@@ -1,5 +1,5 @@
 module.exports={
-    mongoUrl:'mongodb+srv://alekseit422:Saveliy0808@crm.meppcff.mongodb.net/?retryWrites=true&w=majority'
+    mongoUrl:'mongodb+srv://alekseit422:Saveliy0808@crm.meppcff.mongodb.net/crm?retryWrites=true&w=majority'
     // mongoUrl:"mongodb://127.0.0.1:27017/srm"
 }
 // const MongoClient = require("mongodb").MongoClient;
